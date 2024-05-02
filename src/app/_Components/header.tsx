5668d618b18de18d5ece8aecc69c94828e5efa51
behnaz <behnazkeiany@gmail.com>
2024-05-02 14:32:09 +0330

    add header-bottom

--- src/app/_Components/header.tsx
+++ src/app/_Components/header.tsx
@@ -2,10 +2,19 @@ export default function Header({ }) {
     return (
 
         <div className="header">
-            <div className="header-item">Your Cart</div>
-            <div className="header-item">UserInfo</div>
-            <div className="header-item">LOGO</div>
-            <div className="header-item">Socials</div>
+            <div className="header-inner-top">
+                <div className="header-item">Your Cart</div>
+                <div className="header-item">UserInfo</div>
+                <div className="header-item">LOGO</div>
+                <div className="header-item">Socials</div>
+            </div>
+            <div className="header-inner-bottom">
+                <div className="header-item">Mix</div>
+                <div className="header-item">Turk</div>
+                <div className="header-item">Decaff</div>
+                <div className="header-item">Posts</div>
+                <div className="header-item">About Us</div>
+            </div>
         </div>
     )
 }
