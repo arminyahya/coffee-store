85cf2e4a9066072f46a052b36ffdc5fe844c8c62
behnaz <behnazkeiany@gmail.com>
2024-05-02 14:23:12 +0330

    add header

---
+++ src/app/_Components/header.tsx
@@ -0,0 +1,11 @@
+export default function Header({ }) {
+    return (
+
+        <div className="header">
+            <div className="header-item">Your Cart</div>
+            <div className="header-item">UserInfo</div>
+            <div className="header-item">LOGO</div>
+            <div className="header-item">Socials</div>
+        </div>
+    )
+}
